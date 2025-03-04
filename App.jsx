104cd79f277e712c7344b379d86bad27c8261f8f
Asadbek96 <asadbekesemuratov2@gmail.com>
2025-03-04 14:49:06 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './App.css'
+
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <>
+      <h1>Менің визит картам</h1>
+      <div class="info">
+        <h3>Аты-жөні: Есемуратов Асадбек</h3>
+        <p>Мамандығы: Fullstack Developer</p>
+        <p>Тобы:24/8</p>
+        <p>Қала:Almaty, KZ</p>
+      </div>
+      <div class='shadow'></div>
+    </>
+  )
+}
+
+export default App
